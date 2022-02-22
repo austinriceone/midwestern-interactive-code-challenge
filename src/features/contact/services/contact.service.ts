070a4a55bc7aebf55f9c29a52fd8c04f4df64d4c
austinriceone <rice.austin@me.com>
2022-02-22 01:39:19 -0600

    fixing some issues with responsiveness on contact thank you message, removing debugger

--- src/features/contact/services/contact.service.ts
+++ src/features/contact/services/contact.service.ts
@@ -11,7 +11,6 @@ export class ContactService {
   constructor( private http: HttpClient ) { }
 
   sendContactInfo(info: any) {
-    debugger;
     return this.http.post('https://api.mwi.dev/contact', info).pipe(
       map((response) => {
         return response;
